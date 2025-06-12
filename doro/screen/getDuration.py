cabe84c1bf8d9873dd806c03f9a9631fd3961dc3
Balaji01-4D <j.balaji2468@gmail.com>
2025-06-12 08:47:46 +0530

    Refactor GetDuration screen layout and styles; update CSS for consistency

--- doro/screen/getDuration.py
+++ doro/screen/getDuration.py
@@ -1,6 +1,7 @@
+from ast import With
 from textual.screen import ModalScreen
-from textual.widgets import MaskedInput, Label, Input
-from textual.containers import Container, Horizontal, Vertical
+from textual.widgets import MaskedInput, Label, Button
+from textual.containers import Container, Vertical, Horizontal
 
 
 class GetDuration(ModalScreen):
@@ -8,7 +9,15 @@ class GetDuration(ModalScreen):
     def compose(self):
 
         with Container():
+            yield Label("DURATIONS",id="header")
             with Horizontal():
-                yield Label("Duration")
-                yield MaskedInput(template="d9")
-                # yield Input(placeholder="sonething")
+                with Vertical():
+                    yield MaskedInput(template="D0;_",value="25")
+                    yield Label("POMODORO")
+                with Vertical():
+                    yield MaskedInput(template="D0;_",value="5")
+                    yield Label("BREAK")
+                with Vertical():
+                    yield MaskedInput(template="D0;_", value="15")
+                    yield Label("LONG BREAK")
+            yield Button()
